cbe5e353fb8c0e8fb4379677b7fc554dba8c9a67
Tim Wiffen <tim@reclaro.com>
2021-10-11 14:23:02 +0100

    retrieve account method

---
+++ src/Models/Accounts.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ProjectRebel\ActiveCampaign\Models;
+
+use Illuminate\Support\Facades\Http;
+
+class Accounts extends ActiveCampaign
+{
+    public function createContact($data)
+    {
+        return $this->send('POST', '/contacts', $data);
+    }
+
+    public function retrieveAccount(int $accountId)
+    {
+        return $this->send('GET', '/accounts/' . $accountId);
+    }
+
+    // public function listContacts(array $parameters = null)
+    // {
+    //     return $this->send('GET', '/contacts', $parameters);
+    // }
+}
